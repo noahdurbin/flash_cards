ac856a7a89dff7146059ce4f4b5f811b58cfaed9
Noah Durbin <noahdurbin@gmail.com>
2024-03-21 15:45:04 -0600

    added some tests to turn rspec file

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,1 +1,32 @@
 require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+    it 'exists' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn).to be_instance_of(Turn)   
+    end
+
+    it 'contains a guess' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.guess).to eq("Juneau")
+    end
+
+    it 'has a card' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.card).to eq(card)
+    end
+
+    it 'can verify if a guess is correct' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.correct?).to eq(true)
+    end
+end
