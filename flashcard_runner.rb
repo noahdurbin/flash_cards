ce857e55de56fbe6491fe6250315c5d873f173dd
Noah Durbin <noahdurbin@gmail.com>
2024-03-23 19:32:49 -0600

    added a runner file

---
+++ flashcard_runner.rb
@@ -0,0 +1,8 @@
+require './lib/card.rb'
+require './lib/turn.rb'
+require './lib/round.rb'
+require './lib/deck.rb'
+
+#game starts here
+puts "Welcome you are playing with #{@Turns.size} cards"
+puts "-" * 40
