e69fdb0748ad68dcaebbf215aa8cd17ffd1cba97
Noah Durbin <noahdurbin@gmail.com>
2024-03-23 22:10:54 -0600

    defined a start method, discovered I need arguments to initialize the mehtod

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,13 +3,20 @@ require './lib/turn.rb'
 require './lib/round.rb'
 require './lib/deck.rb'
 
-    question_1 = Card.new("How many bones are there in the human body?", "206", :Biology)
-    question_2 = Card.new("What is the name of the longest river in South America?", "Amazon River", :Geography)
-    question_3 = Card.new("What does Na stand for on the periodic table?", "Salt", :Science)
-    question_4 = Card.new("What temperature (in Fahrenheit) does water freeze at?", "32 Degrees", :Science)
+question_1 = Card.new("How many bones are there in the human body?", "206", :Biology)
+question_2 = Card.new("What is the name of the longest river in South America?", "Amazon River", :Geography)
+question_3 = Card.new("What does Na stand for on the periodic table?", "Salt", :Science)
+question_4 = Card.new("What temperature (in Fahrenheit) does water freeze at?", "32 Degrees", :Science)
 
-    deck_1 = Deck.new([question_1, question_2, question_3, question_4])
+deck_1 = Deck.new([question_1, question_2, question_3, question_4])
+
+round_1 = Round.new(deck_1)
 
 #game starts here
-puts "Welcome you are playing with cards"
 
+def start(round_1, deck_1)
+    puts "Welcome! You're playing with #{deck_1.count} cards."
+    puts "-" * 40
+end
+
+start(round_1, deck_1)
