07de968fcbd110c43d3359a324603b765a584b9d
Noah Durbin <noahdurbin@gmail.com>
2024-03-26 14:02:19 -0600

    added initialize and file data method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,24 @@
 class CardGenerator 
-    attr_reader 
+    attr_reader :file
+                
+    attr_accessor :@deck
 
-    def initialize
+    def initialize(file)
+        @file = file.open
+        @deck = []
+    end
+
+    def file_data
+        @file.readlines.map(:&chomp)
+    end
+
+    def generate_cards
 
     end
+
+
 end
+
+# this class is initiated by a new round with an argument of a txt file that has questions, answers, and categories
+# when a new instance of this class is initiated I want a file to be read and values taken out of it creating new cards
+# then I want those card to be put in a new deck of cards
